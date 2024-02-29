199b335786433647e76c594e69b5566de02a79a5
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-02-29 12:22:52 +0530

    Done the Navbar...

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -11,6 +11,7 @@ import { HiOutLineBars3, HiOutlineBars3 } from 'react-icons/hi2';
 import { 
     Box,
     Drawer,
+    List,
     ListItem,
     ListItemButton,
     ListItemIcon,
@@ -28,7 +29,7 @@ import ShoppingCartRoundedIcon from '@mui/icons-material/ShoppingCartRounded';
 const Navbar = () => {      
     const[openMenu,setOpenMenu] = useState(false);
 
-    const menuOptions = [
+    const menuOptions = [                                                                                                           
         {
             text:"Home",
             icon:<HomeIcon />
@@ -69,6 +70,28 @@ const Navbar = () => {
             <div className='navbar-menu-container'>
                 <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
             </div>
+            <Drawer open={openMenu} onClose={() => setOpenMenu(false)}
+            anchor='right'>
+                <Box
+                    sx={{ width: 250 }}
+                    role='presentation'
+                    onClick={() => setOpenMenu(false)}
+                    onKeyDown={() => setOpenMenu(false)}
+                >
+                    <List>
+                        {
+                            menuOptions.map((item => (
+                                <ListItem key={item.text} disablePadding>
+                                    <ListItemButton>
+                                        <ListItemIcon>{item.icon}</ListItemIcon>
+                                        <ListItemText>{item.text}</ListItemText>
+                                    </ListItemButton>
+                                </ListItem>
+                            )))
+                        }
+                    </List>
+                </Box>
+            </Drawer>
         </nav>
     )
 }
